888322a31ce76a8a9ff5e55997b865e0a0a9e680
shubhamdyadav <shubhamdyadav17@gmail.com>
2025-07-07 23:38:46 +0530

    added the bike inc

--- git-practise/src/app/app.module.ts
+++ git-practise/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MotorIncComponent } from './motor-inc/motor-inc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MotorIncComponent
   ],
   imports: [
     BrowserModule,
